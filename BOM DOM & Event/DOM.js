c9de6ace795407a63ba36f52e800380e3f14621f
Aryodjat1 <aryodjati5@gmail.com>
2022-10-16 14:24:11 +0700

    finished DOM course

--- BOM DOM & Event/DOM.js
+++ BOM DOM & Event/DOM.js
@@ -9,7 +9,7 @@ const body = document.body;
 body;
 
 //get element by ID
-const gambar = document.getElementById('gambarUtama');  
+const gambar1 = document.getElementById('gambarUtama');  
 
 // CREATE ELEMENT ------------------------------------------------------------------------------------------------------------------------
 
@@ -25,4 +25,34 @@ newImg.setAttribute('src', 'https://picsum.photos/200/300'); // ADD IMG
 
 // MANIPULATION ELEMENT ------------------------------------------------------------------------------------------------------------------------
 
-// GET ELEMENT ------------------------------------------------------------------------------------------------------------------------
+element.setAttribute('nama_atribut_sasaran', 'nilai_atribut_sasaran');// format
+const gambar = document.getElementById('gambar'); // ubah DOM.HTML
+gambar.setAttribute('width', 300);
+gambar.setAttribute('height', 215);
+const buttons = document.querySelectorAll('.button');
+const playButton = buttons[3];
+const playButtonElement = playButton.children[0];
+
+// MANIPULATION CONTENT ------------------------------------------------------------------------------------------------------------------------
+
+dicoding.innerHTML = '<i>Belajar Programming di Dicoding</i>';// INNER HTML
+
+const buttons1 = document.getElementsByClassName('button');
+// MANIPULATING STYLE
+for (const button of buttons) {
+    button.children[0].style.borderRadius = '6px';
+  }
+
+// INSERT TO HTML ------------------------------------------------------------------------------------------------------------------------
+
+// APPEND CHILD
+const newElement = document.createElement('li');
+newElement.innerText = 'Selamat menikmati!';
+const daftar = document.getElementById('daftar');
+daftar.appendChild(newElement);
+
+//INSERT BEFORE
+const elementAwal = document.createElement('li');
+elementAwal.innerText = 'Hidupkan kompor.';
+const itemAwal = document.getElementById('awal');
+daftar.insertBefore(elementAwal, itemAwal);
