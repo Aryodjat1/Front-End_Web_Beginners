bacc50ef937a66a7ea35fa2a07ef9c7283904ce1
Aryodjat1 <aryodjati5@gmail.com>
2022-10-18 22:32:08 +0700

    resume todoapps

---
+++ bookshelf-apps/main.js
@@ -0,0 +1,11 @@
+//inisialiasi variabel untuk menampung elemen dokumen
+const inputBookTitle = document.getElementById('inputBookTitle');
+const inputBookAuthor = document.getElementById('inputBookAuthor');
+const inputBookYear = document.getElementById('inputBookYear');
+const inputBookIsComplete = document.getElementById('inputBookIsComplete')
+const bookSubmit = document.getElementById('bookSubmit');
+const searchBookTitle = document.getElementById('searchBookTitle');
+const searchSubmit = document.getElementById('searchSubmit');
+const incompleteBookshelfList = document.getElementById('incompleteBookshelfList');
+const completeBookshelfList = document.getElementById('completeBookshelfList');
+
